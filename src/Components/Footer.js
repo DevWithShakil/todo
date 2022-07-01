2f1cb4c08e9a0ecdf25d46396388a77fb5f17faa
Shakil Khan <m.shakilkhan720@gmail.com>
2022-07-01 10:30:24 +0600

    navbar and todo added

---
+++ src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='footer1'>
+
+            </div>
+            <footer class="footer footer-center p-4text-base-content">
+                <div>
+                    <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../Footer';
+import ToDo from '../ToDo/Todo';
+
+const Home = () => {
+    return (
+        <div>
+            <ToDo />
+        </div>
+    );
+};
+
+export default Home;
